c85f0a461db50c75a7dee0e79f9ef5c852b54d9c
udion <uddeshya.upa@gmail.com>
2017-05-31 12:58:29 +0800

    method to perform CPA on the traces using the model

---
+++ src/cpa.jl
@@ -0,0 +1,41 @@
+include("model.jl")
+
+type cpaResults
+  key
+  ρ::Array
+  keyGraphs::Array
+  pointOfinterests::Array #the time instants at which the activity for the byte happened
+end
+
+function cpa(attack_round, model_type, cipher_type, trace_data)
+  V = modelmaker(attack_round, model_type, cipher_type, trace_data)
+
+  num_unit = trace_data.num_unit
+  unit_size = trace_data.unit_size
+  keymax = 2^unit_size -1
+  num_trace = length(trace_data.traces[1,:])
+
+  ρ = zeros(num_unit,keymax+1,num_trace)
+
+  for i in 1:num_unit
+    for keyGuess in 0:keymax
+      for t in 1:num_trace
+        ρ[i,keyGuess+1,t] = abs(cor(V[i,:,keyGuess+1], trace_data.traces[:,t]))
+      end
+    end
+  end
+
+  #now I have the final matrix, now to extract information
+  #to recover key
+  recovered_key = ""
+  keyGraphs = Array{}[]
+  poi = Int[]
+  for i in 1:num_unit
+    push!(keyGraphs, findmax(ρ[i,:,:],2)[1])
+    push!(poi, findmax(findmax(ρ[i,:,:],2)[1])[2])
+    predict_k = findmax(findmax(ρ[i,:,:],2)[1])[2]-1
+    #println(predict_k)
+    recovered_key = join([recovered_key,hex(predict_k,div(unit_size,4))])
+  end
+  res = cpaResults(recovered_key, ρ, keyGraphs, poi)
+end
