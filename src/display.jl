7242e37600f95911280b18f5811502d63c214d55
udion <uddeshya.upa@gmail.com>
2017-06-22 01:17:35 +0800

    plots directly saved to directory

--- src/display.jl
+++ src/display.jl
@@ -8,13 +8,14 @@ function display_cpa(results)
   keymax = length(results.ρ[1,:,1]) - 1
   #to display the graphs for each key unit guessed
   for i in 1:length(results.keyGraphs)
-    plt.figure()
+    fig = plt.figure()
     plt.plot(results.keyGraphs[i], color="maroon")
     plt.xlim(0,keymax)
     xlabel("key byte values")
     ylabel("maximum correlation values")
     title("max correlation vs key values for unit$(i)")
     plt.savefig(join([results.res_dir, "/max_cc_keyvals_for_byte$(i)"]))
+    plt.close(fig)
   end
   #can add more functions to manipulate ρ and show details
 end
