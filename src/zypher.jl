29a4a7dd3414427499d2898c80189f88807287a0
udion <uddeshya.upa@gmail.com>
2017-06-25 19:59:13 +0800

    updates exported

--- src/zypher.jl
+++ src/zypher.jl
@@ -1,5 +1,5 @@
 module zypher
   include("display.jl")
-  export display_cpa, cpa, cpaResults, buildTemplates, ta, get_pois
+  export display_cpa, display_dpa, display_ta, cpa, buildTemplates, ta, get_pois
   export SPNdata, loadSPNtrace, loadTA_TRAINtrace, loadTA_TESTtrace
 end
