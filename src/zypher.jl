5ecffaf85cc3b26f00afcce545000b2b341057d2
udion <uddeshya.upa@gmail.com>
2017-06-25 18:09:36 +0800

    template related methods exported

--- src/zypher.jl
+++ src/zypher.jl
@@ -1,5 +1,5 @@
 module zypher
   include("display.jl")
-  export display_cpa, cpa, cpaResults, templatemaker1
+  export display_cpa, cpa, cpaResults, buildTemplates, ta, get_pois
   export SPNdata, loadSPNtrace, loadTA_TRAINtrace, loadTA_TESTtrace
 end
